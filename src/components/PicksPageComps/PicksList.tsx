d294f51f94cd54a5a3b3fc2f02317843b3a99a5f
byoon97 <byoon5397@gmail.com>
2024-06-11 15:29:43 -0400

    test

--- src/components/PicksPageComps/PicksList.tsx
+++ src/components/PicksPageComps/PicksList.tsx
@@ -99,6 +99,7 @@ export default function PicksList({ page }: Props) {
         <div className="grid grid-cols-1 md:grid-cols-2 gap-3 lg:hidden">
           {!loading &&
             slate.map((pick: SinglePickProps) => {
+              console.log(pick);
               const generateLink = () => {
                 let link;
                 const awayTeam = pick.awayTeam.replace(/\s/g, "_");
