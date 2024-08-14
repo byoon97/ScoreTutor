4e9d262d94cd62aeab4d70594f9c99c2ac639bb6
byoon97 <byoon5397@gmail.com>
2024-08-13 21:27:19 -0400

    changes

--- lib/apollo-wrapper.tsx
+++ lib/apollo-wrapper.tsx
@@ -8,7 +8,7 @@ import {
 
 export function makeClient() {
   const httpLink = new HttpLink({
-    uri: "https://scoretutor.vercel.app/api/graphql",
+    uri: "https://betterbettor.me/api/graphql",
     // uri: "http://localhost:3000/api/graphql",
     credentials: "same-origin",
   });
